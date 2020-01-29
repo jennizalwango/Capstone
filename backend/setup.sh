fc316a6bb00634e39a53a43038f7a7c46cc74319
jennizalwango <jennyzalwango12@gmail.com>
2020-01-29 14:46:25 +0300

    add runtime.txt

---
+++ backend/setup.sh
@@ -0,0 +1,12 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    """
+    Base application configuration
+    """
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'thisissecret')
+
+SQLALCHEMY_DATABASE_URI = 'postgres://aynfsfqgbdvyfp:a9d37d905a49931414b728b2e754d910a149028de26647eee8d2abf5e9e07b33@ec2-54-174-221-35.compute-1.amazonaws.com:5432/d8fqcfq8mq61n7'
